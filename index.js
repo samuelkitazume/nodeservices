b4c6b8686558767de53e49e2858a1f3caa9e9961
Samuel Kitazume <samuel.kitazume@gmail.com>
2016-08-09 21:45:27 -0300

    Better folder structure

--- index.js
+++ index.js
@@ -1,32 +1,4 @@
 #! /usr/bin/env node
 'use strict';
 
-var commandLineArgs = require("command-line-args");
-var args_definitions = [{
-	name: "registry",
-	alias: "r",
-	type: Boolean
-},{
-	name: "discovery",
-	alias: "d",
-	type: Boolean
-},{
-	name: "registry-port",	
-	type: String,
-	defaultValue: 4001
-},{
-	name: "discovery-port",	
-	type: String,
-	defaultValue: 4002
-}];
-
-var args = commandLineArgs(args_definitions);
-
-if (args.registry) {	
-	require("./registry.js")(args["registry-port"]);
-}
-if (args.discovery) {
-	require("./discovery.js")(args["discovery-port"]);
-}
-
-console.log("Everything is right. Be cool.");
+require("./lib/nodeservices")();
